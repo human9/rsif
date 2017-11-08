c7ead215f246921fd6517d82eb521ed1d5abcaca
John Salamon <john.salamon@sahmri000435.local>
2017-11-08 14:36:18 +1030

    hmm

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+
+extern crate petgraph;
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|e| {
@@ -24,6 +30,7 @@ fn run(config: Config) -> Result<(), Box<Error>> {
 
     match config.operation.as_ref() {
         "nodes" => println!("{:?}", nodes(&contents)),
+        "test" => println!("{:?}", petgraph::dot::Dot::new(&sif_to_petgraph(&contents).graph)),
         _ => println!("Unimplemented operation"),
     }
 
@@ -49,6 +56,49 @@ fn nodes<'a>(contents: &'a String) -> HashSet<&'a str> {
         })
 }
 
+struct MappedGraph<'a> {
+    map: HashMap<&'a str, NodeIndex<u32>>,
+    graph: Graph<&'a str, &'a str, petgraph::Undirected, u32>,
+}
+
+impl<'a> MappedGraph<'a> {
+    pub fn new() -> Self {
+        MappedGraph {
+            map: HashMap::new(),
+            graph: Graph::new_undirected(),
+        }
+    }
+}
+
+/// Convert a sif file into a petgraph graph
+fn sif_to_petgraph<'a>(contents: &'a String) -> MappedGraph {
+    contents.lines()
+        .enumerate()
+        .filter_map(|(i, line)| {
+            let mut tokens: Vec<&str> = line.split('\t').collect();
+            if tokens.len() !=3 {
+                println!("Problem parsing line {}", i);
+                return None
+            }
+            tokens.swap(1, 2);
+            return Some(tokens)
+        }).fold(MappedGraph::new(), |mut graph, t| {
+            let nodes: Vec<NodeIndex<u32>> = t.iter().take(2).map(|name| {
+                match graph.map.entry(name) {
+                    Occupied(entry) => {
+                        *entry.get()
+                    }
+                    Vacant(entry) => {
+                        let index = graph.graph.add_node(name);
+                        entry.insert(index);
+                        index
+                    }
+                }
+            }).collect();
+            graph.graph.update_edge(nodes[0], nodes[1], t[2]);
+            graph
+        })
+}
 
 struct Config {
     operation: String,
@@ -68,7 +118,7 @@ impl Config {
 
         match operation.as_ref() {
             "union" | "intersection" => { if files.len() < 2 { return Err("Too few files") } },
-            "nodes" | "edges" => { if files.len() < 1 { return Err("Please specify a file") } },
+            "nodes" | "edges" | "test" => { if files.len() < 1 { return Err("Please specify a file") } },
             _ => return Err("Unknown operation"),
         }
             
